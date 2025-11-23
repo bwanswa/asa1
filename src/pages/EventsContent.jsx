d478539de095d4927186d45c647c46225e28097b
bwanswa <deogratiousbwanswa@gmail.com>
2025-11-23 09:29:11 -0800

    Delete src/pages directory

--- src/pages/EventsContent.jsx
+++
@@ -1,9 +0,0 @@
-// pages/EventsContent.jsx
-import React from 'react';
-
-export const EventsContent = () => (
-    <div style={{ padding: 20, color: "white", background: "#111", flex: 1 }}>
-        <h2>🎉 Events</h2>
-        <p>Upcoming events will appear here. This data can also be loaded from a public Firestore collection!</p>
-    </div>
-);
